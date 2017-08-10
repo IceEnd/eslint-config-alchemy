a9f38c009742e0e50674529d81faf39680acf30c
mindanfeng <mindanfeng@jd.com>
2017-08-10 16:23:06 +0800

    修改no-restricted-syntax规则

--- rules/style.js
+++ rules/style.js
@@ -239,10 +239,6 @@ module.exports = {
     'no-restricted-syntax': [
       'error',
       {
-        selector: 'ForInStatement',
-        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
-      },
-      {
         selector: 'ForOfStatement',
         message: 'iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations.',
       },
